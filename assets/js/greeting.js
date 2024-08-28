9c89643d3a52a33e8fcb1c8106ed1cfccff934d1
huyhoangg9owl <thh241005@gmail.com>
2024-08-28 17:22:11 +0700

    Change greeting js file (again)

--- assets/js/greeting.js
+++ assets/js/greeting.js
@@ -13,15 +13,15 @@ const gree3 = `${CONFIG.greetingAfternoon}\xa0`;
 const gree4 = `${CONFIG.greetingEvening}\xa0`;
 
 if (hour >= 23 || hour < 6) {
-  document.getElementById("greetings").innerText =
+  document.getElementById("greetings").innerHTML =
     `<center>${name}<br>${gree1}</center>`;
 } else if (hour >= 6 && hour < 12) {
-  document.getElementById("greetings").innerText =
+  document.getElementById("greetings").innerHTML =
     `<center>${name}<br>${gree2}</center>`;
 } else if (hour >= 12 && hour < 17) {
-  document.getElementById("greetings").innerText =
+  document.getElementById("greetings").innerHTML =
     `<center>${name}<br>${gree3}</center>`;
 } else {
-  document.getElementById("greetings").innerText =
+  document.getElementById("greetings").innerHTML =
     `<center>${name}<br>${gree4}</center>`;
 }
